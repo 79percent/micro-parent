6c7959bc32ec69490a3140b10165de3e1f0dca18
per <per7614@163.com>
2020-07-09 17:25:25 +0800

    7.9导航

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,81 @@
 import React from 'react';
-import { Menu } from 'antd';
-import {history} from 'umi';
-export default (props: any) => {
-  return <div style={{ width: '100%', height: '100%', display: 'flex' }}>
-    <div style={{ width: 256 }}>
-      <Menu
-        defaultSelectedKeys={['0']}
-        defaultOpenKeys={['sub0']}
-        mode="inline"
-      >
-        <Menu.Item key="0" onClick={()=>history.push('/')}>
-          主应用
-        </Menu.Item>
-        <Menu.Item key="1" onClick={()=>history.push('/app1')}>
-          App1
-        </Menu.Item>
-        <Menu.Item key="2" onClick={()=>history.push('/app2')}>
-          App2
-        </Menu.Item>
-      </Menu>
-    </div>
-    {props.children}
-  </div>;
+import { Menu, Avatar, Breadcrumb } from 'antd';
+import { history } from 'umi';
+import {
+  HomeOutlined,
+} from '@ant-design/icons';
+import reactSvg from '../assets/react.svg';
+import vueSvg from '../assets/vue.svg';
+import styles from './styles/index';
+import './styles/css.less'
+
+const breadCrumbMap = {
+  'home': '首页',
+  'react': 'React',
+  'vue': 'Vue'
+}
+
+export default class App extends React.Component {
+  state = {
+    current: 'home',
+    breadcrumb: '首页'
+  };
+
+  handleClick = (e: { key: any; }) => {
+    console.log('click ', e);
+    const { key } = e
+    this.setState({
+      current: key,
+      breadcrumb: breadCrumbMap[key]
+    });
+  };
+
+  render() {
+    const { current, breadcrumb } = this.state;
+    return (
+      <div style={styles.box}>
+        <div style={styles.nav}>
+          <div style={styles.navLeft}>
+            <Avatar size={40} src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+          </div>
+          <Menu
+            mode="horizontal"
+            style={styles.menu}
+            onClick={this.handleClick}
+            selectedKeys={[current]}
+          >
+            <Menu.Item key="home" onClick={() => history.push('/')} style={styles.menuItembox}>
+              <div style={styles.menuItem}>
+                <HomeOutlined style={styles.menuIcon} />
+                <span style={styles.menuItemText}>首页</span>
+              </div>
+            </Menu.Item>
+            <Menu.Item key="react" onClick={() => history.push('/app1')} style={styles.menuItembox}>
+              <div style={styles.menuItem}>
+                <img src={reactSvg} style={styles.menuIcon}></img>
+                <span style={styles.menuItemText}>React</span>
+              </div>
+            </Menu.Item>
+            <Menu.Item key="vue" onClick={() => history.push('/app2')} style={styles.menuItembox}>
+              <div style={styles.menuItem}>
+                <img src={vueSvg} style={styles.menuIcon}></img>
+                <span style={styles.menuItemText}>Vue</span>
+              </div>
+            </Menu.Item>
+            {/* <Menu.Item key="zhihuinl" style={styles.menuItembox}>
+              <a style={styles.menuItem} href="http://www.test7.club:8080" target="_blank" rel="noopener noreferrer">
+                智慧农林网
+                </a>
+            </Menu.Item> */}
+          </Menu>
+        </div>
+        <div style={styles.content}>
+          <Breadcrumb>
+            <Breadcrumb.Item><h3>{breadcrumb}</h3></Breadcrumb.Item>
+          </Breadcrumb>
+          {this.props.children}
+        </div>
+      </div>
+    );
+  }
 }
---
+++ src/pages/styles/index.ts
@@ -0,0 +1,48 @@
+const styles = {
+  box: {
+    width: '100%',
+    height: '100%'
+  },
+  nav: {
+    display: 'flex',
+    justifyContent: 'flex-start',
+    flex: 5
+  },
+  navLeft: {
+    display: 'flex',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  menu: {
+    flex: 4,
+    height: 60
+  },
+  menuItembox: {
+    height: '100%'
+  },
+  menuItem: {
+    display: 'flex',
+    alignItems: 'center',
+    height: '100%'
+  },
+  menuItemText: {
+    fontSize: 16,
+  },
+  menuIcon: {
+    fontSize: 20,
+    width: 20,
+    height: 20,
+    display: 'inline-block',
+    marginRight: 10
+  },
+  content: {
+    paddingLeft: 80,
+    paddingRight: 80,
+    paddingTop: 40,
+    paddingBottom: 40,
+    backgroundColor: '#ccc',
+    height: '100%'
+  }
+}
+export default styles
